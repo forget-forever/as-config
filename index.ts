0dae4d4fe127aa9605164d05cbcb600404ae10db
周美磊 <ZHOUMEILEI@SFMAIL.SF-EXPRESS.COM>
2022-03-11 19:24:03 +0800

    增加index.ts文件

--- index.ts
+++ index.ts
@@ -1,20 +1,33 @@
 /*
  * @Author: zml
  * @Date: 2022-03-11 19:14:52
- * @LastEditTime: 2022-03-11 19:15:20
+ * @LastEditTime: 2022-03-11 19:18:34
  */
 import { ApiDetail } from "./detailType"
 
 export type ConfigApi = {
-  url: string, // 接口的url
-  paramsType: string, // query请求参数类型
-  dataType: string, // 请求体的参数类型
-  returnType: string, // 返回的结果类型
-  method: string, // 请求方式
-  paramsHandle: (paramsType?: string, dataType?: string, params?: string, data?: string) => string, // 参数的预处理
-  requestDataHandle: (paramsType?: string, dataType?: string, params?: string, data?: string) => string, // 请求参数的预处理
-  urlHandle: (url: string, params?: string) => string, // 内置的路径预处理函数，处理路由传参
-  apiDetail: ApiDetail<'obj'> // 接口的详情
+  /** 接口的url */
+  url: string,
+  /** query请求参数类型 */
+  paramsType: string,
+  /** 请求体的参数类型 */
+  dataType: string,
+  /** 返回的结果类型 */
+  returnType: string,
+  /** 请求方式 */
+  method: string,
+  /**
+   * 参数的预处理方法
+   */
+  paramsHandle: (paramsType?: string, dataType?: string, params?: string, data?: string) => string,
+  /**
+   * 请求参数的预处理
+   */
+  requestDataHandle: (paramsType?: string, dataType?: string, params?: string, data?: string) => string,
+  /** 内置的路径预处理函数，处理路由传参 */
+  urlHandle: (url: string, params?: string) => string,
+  /** 接口的详情, yapi的接口详情的数据 */
+  apiDetail: ApiDetail<'obj'> 
 }
 
 export type AsConfig = {
@@ -22,7 +35,7 @@ export type AsConfig = {
   projectId: string,
   /** 项目token */
   token: string,
-  /** yapi的mock地址，写上域名就够了，例如：http://yapi.sfjswl.com */
+  /** yapi的mock地址，写上域名就够了，例如：http://yapi.xxxxxxx.com */
   mockUrl?: string,
   /** 引入的model */
   importModel?: string[],
@@ -30,17 +43,23 @@ export type AsConfig = {
   collections?: Record<string, string[]>,
   /** 生成请求的目录 */
   outPath?: string,
-  /** */
+  /** 生成的方法模版` */
   serviceTemplate?: (api: ConfigApi) => string,
   /** 类型文件中引入的model */
   importTypeModel?: string[],
   /** 返回的参数解析类型的节点，默认是data节点开始解析 */
   typeRootNode?: string,
-  /** 生成的文件的拓展名，分为.js 和 .ts */
-  extendName?: string,
+  /**
+   * 生成的文件的拓展名，分为.js 和 .ts
+   * @default .ts
+   */
+  extendName?: '.js' | '.ts',
   /** 是否需要生成ts类型, 默认为true */
   tsType?: boolean,
-  /** 类型的导出形式，分为 declare 和 export两种 */
+  /**
+   * 类型的导出形式，分为 declare 和 export两种
+   * @default export
+   */
   exportType?: 'export' | 'declare',
   /** 类型的命名空间 */
   typeNamespace?: string,
